de3106fd018fd38cbea33d609c3c9825d36158cb
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-07-14 11:54:14 -0400

    finished the intro tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ItemComponent } from './item/item.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ItemComponent,
+    ProductListingComponent
   ],
   imports: [
     BrowserModule
